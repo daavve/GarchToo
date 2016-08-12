b04e3b5d887da12809cd56fbad2b3811c25e1dbe
David McInnis <davidm@eagles.ewu.edu>
2016-08-12 13:54:02 -0700

    added some changes

--- runner-embeded-list-2.py
+++ runner-embeded-list-2.py
@@ -10,7 +10,7 @@
 
 
 
-pkg_list = "gnuplot:5.0.4-1 libksba:1.3.4-2 gpgme:1.6.0-3 groff:1.22.3-7 mesa:12.0.1-6 gtk3:3.20.6-2 guile:2.0.12-1 kde-cli-tools:5.7.3-1 libmm-glib:1.6.0-1 libnl:3.2.28-1 libsodium:1.0.11-1 libsynctex:2016.41290-2.1 libzip:1.1.3-1 mercurial:3.9-1 mesa-vdpau:12.0.1-6 mpg123:1.23.6-1 openssh:7.3p1-1 python-beautifulsoup4:4.5.1-1 python-coverage:4.2-1 python-matplotlib:1.5.2-1 python-setuptools:1:25.1.3-1 python2-setuptools:1:25.1.3-1 tcl:8.6.6-1 texlive-bin:2016.41290-2.1 texlive-bibtexextra:2016.41470-2 texlive-latexextra:2016.41473-2 texlive-humanities:2016.41380-2 texlive-langchinese:2016.41405-2 texlive-langcyrillic:2016.41231-2 texlive-langgreek:2016.40850-2 texlive-music:2016.41389-2 texlive-pictures:2016.41299-2 texlive-plainextra:2016.41437-2 texlive-pstricks:2016.41321-2 texlive-publishers:2016.41474-2 texlive-science:2016.41327-2 tk:8.6.6-1 valgrind:3.11.0-4 vim-runtime:7.4.2143-1 vim:7.4.2143-1 xfce4-notifyd:0.3.0-1 xfce4-power-manager:1.6.0-1"
+pkg_list = "libcups:2.1.4-2 fontconfig:2.12.1-1 openjpeg2:2.1.1-2 libinput:1.4.1-1 poppler:0.46.0-2 cups-filters:1.10.0-2 cups:2.1.4-2 fakeroot:1.21-2 glew:2.0.0-1 gtk-update-icon-cache:3.20.8-1 gtk3:3.20.8-1 gvfs:1.28.2+22+g6c1124d-1 hugin:2016.0.0-4 inetutils:1.9.4-4 libnm-glib:1.2.4-1 libreoffice-fresh:5.2.0-2 libsynctex:2016.41290-4 mesa-demos:8.3.0-2 net-tools:1.60.20160710git-1 networkmanager:1.2.4-1 poppler-glib:0.46.0-2 poppler-qt4:0.46.0-2 poppler-qt5:0.46.0-2 python-setuptools:1:25.1.5-1 python2-setuptools:1:25.1.5-1 texlive-bin:2016.41290-4 texlive-fontsextra:2016.41439-2 texlive-formatsextra:2016.41438-2 texlive-games:2016.39318-2 texlive-genericextra:2016.41413-2 texlive-htmlxml:2016.41440-2 vte:0.28.2-8 xf86-video-intel:1:2.99.917+691+ga77397a-1"
 
 import subprocess
 from collections import deque
@@ -18,9 +18,9 @@ import os
 import socket
 from multiprocessing.managers import BaseManager
 import queue
-inport time
+import time
 
-f = open("failed2.txt", "w")
+f = open("failed4.txt", "w")
 queue = queue.Queue()
 class QueueManager(BaseManager): pass
 QueueManager.register('get_queue', callable=lambda:queue)
