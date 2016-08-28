149f0f323cc28942b1f1abc7ab5ae0ca52d43b2d
David McInnis <davidm@eagles.ewu.edu>
2016-08-28 09:28:12 -0700

    pkg deps not exclude optdeps and build a correct list

--- parsepkgbuild.py
+++ parsepkgbuild.py
@@ -2,6 +2,7 @@
 #
 # Gets a pkgbuild and builds a list of required packages
 # Also returns a list of pkgs build by this PKGBUILD
+# Might be better to build tokenizer to go over regexes
 #
 ##########################################################################
 
@@ -20,17 +21,18 @@ bld_pkgs = []
 built_pkgs = subprocess.run(["makepkg", "--printsrcinfo"], stdout=subprocess.PIPE, universal_newlines=True).stdout.split("\n\n")
 # First part gets base stuff
 for baseP in built_pkgs[0].split("\n"):
-    if "depends = " in baseP:
+    if "depends = " in baseP and "optdepends =" not in baseP:
         pkgs.append(baseP.strip().split("=")[1])
 for i in range(1, len(built_pkgs) - 1):
     built_pkg = built_pkgs[i].split("\n")
     bld_pkgs2 = pkgs.copy()
     pkg_name = built_pkg[0].strip().split("=")[0]
-    for builtP in built_pkgs:
-        if "depends = " in builtP:
+    for builtP in built_pkg:
+        if "depends = " in builtP and "optdepends =" not in builtP:
             bld_pkgs2.append(builtP.strip().split("=")[1])
     bld_pkgs.append(Deps(pkg_name, bld_pkgs2))
 
 
-print(bld_pkgs)
+#for pkg in bld_pkgs:
+#    print(pkg.pkg_deps)
         
